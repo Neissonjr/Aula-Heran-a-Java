240888145bfd4f2d7587ab771bde4febc074d0e3
Felipe Gravel <gravelfelipe@gmail.com>
2025-09-20 00:15:38 +0000

    feat: criar classe moto

--- src/main/java/equipe/Moto.java
+++ src/main/java/equipe/Moto.java
@@ -1,5 +1,28 @@
 package equipe;
 
 public class Moto extends Veiculo {
+    private int cilindradas;
 
+	public Moto(String marca, String modelo, int ano, int cilindradas) {
+		super(marca, modelo, ano);
+		this.cilindradas = cilindradas;
+	}
+
+	public int getCilindradas() {
+		return cilindradas;
+	}
+
+	public void setCilindradas(int cilindradas) {
+		this.cilindradas = cilindradas;
+	}
+
+	@Override
+	public void acelerar() {
+		System.out.println(getMarca() + " é uma marca de moto que acelera rápido!");
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " - Moto com "+ cilindradas + " cilindradas";
+	}
 }
